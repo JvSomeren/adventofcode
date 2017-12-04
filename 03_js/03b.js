5824956b6edba927265e450dfc289c6ec347c758
Joost van Someren <joost.v.someren@gmail.com>
2017-12-04 22:28:23 +0100

    Complete part 2 day 03

---
+++ 03_js/03b.js
@@ -0,0 +1,102 @@
+part2 = () => {
+
+  calculateValue = (spiral, pos) => {
+    var value = 0;
+
+    if(spiral[(pos.x-1) + ':' + (pos.y-1)] !== undefined)
+      value += spiral[(pos.x-1) + ':' + (pos.y-1)];
+
+    if(spiral[(pos.x-1) + ':' + (pos.y)] !== undefined)
+      value += spiral[(pos.x-1) + ':' + (pos.y)];
+
+    if(spiral[(pos.x-1) + ':' + (pos.y+1)] !== undefined)
+      value += spiral[(pos.x-1) + ':' + (pos.y+1)];
+
+    if(spiral[(pos.x) + ':' + (pos.y-1)] !== undefined)
+      value += spiral[(pos.x) + ':' + (pos.y-1)];
+
+    if(spiral[(pos.x) + ':' + (pos.y+1)] !== undefined)
+      value += spiral[(pos.x) + ':' + (pos.y+1)];
+
+    if(spiral[(pos.x+1) + ':' + (pos.y-1)] !== undefined)
+      value += spiral[(pos.x+1) + ':' + (pos.y-1)];
+
+    if(spiral[(pos.x+1) + ':' + (pos.y)] !== undefined)
+      value += spiral[(pos.x+1) + ':' + (pos.y)];
+
+    if(spiral[(pos.x+1) + ':' + (pos.y+1)] !== undefined)
+      value += spiral[(pos.x+1) + ':' + (pos.y+1)];
+
+    return value;
+  };
+
+  moveRight = (move, spiral, pos) => {
+    pos.x += 1;
+
+    spiral[pos.x + ':' + pos.y] = calculateValue(spiral, pos);
+
+    if(spiral[pos.x + ':' + (pos.y+1)] === undefined)
+      move[0] = 'u';
+  };
+
+  moveUp = (move, spiral, pos) => {
+    pos.y += 1;
+
+    spiral[pos.x + ':' + pos.y] = calculateValue(spiral, pos);
+
+    if(spiral[(pos.x-1) + ':' + pos.y] === undefined)
+      move[0] = 'l';
+  };
+
+  moveLeft = (move, spiral, pos) => {
+    pos.x -= 1;
+
+    spiral[pos.x + ':' + pos.y] = calculateValue(spiral, pos);
+
+    if(spiral[pos.x + ':' + (pos.y-1)] === undefined)
+      move[0] = 'd';
+  };
+
+  moveDown = (move, spiral, pos) => {
+    pos.y -= 1;
+
+    spiral[pos.x + ':' + pos.y] = calculateValue(spiral, pos);
+
+    if(spiral[(pos.x+1) + ':' + pos.y] === undefined)
+      move[0] = 'r';
+  };
+    
+  const part2 = document.getElementById("part2");
+
+  const input = 347991;
+  
+  var spiral = {
+    '0:0': 1
+  };
+
+  var pos = {
+    x: 0,
+    y: 0
+  };
+
+  var move = ['r'];
+  while(spiral[pos.x + ':' + pos.y] < input) {
+    switch(move[0]) {
+      case 'r':
+        moveRight(move, spiral, pos);
+        break;
+      case 'u':
+        moveUp(move, spiral, pos);
+        break;
+      case 'l':
+        moveLeft(move, spiral, pos);
+        break;
+      case 'd':
+        moveDown(move, spiral, pos);
+        break;
+    }
+  }
+
+  part2.innerHTML = spiral[pos.x + ':' + pos.y];
+
+}
--- 03_js/README.md
+++ 03_js/README.md
@@ -1,2 +1,5 @@
 # Day 3: Spiral Memory
 Solution for [day 3](https://adventofcode.com/2017/day/3) of the Advent of Code challenge.
+
+## Run
+Open `03.html` in any browser.
