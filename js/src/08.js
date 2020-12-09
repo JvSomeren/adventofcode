91b8a0ae90c8f4ea3dadf01867c515343ea788e3
Joost van Someren <joost.v.someren@gmail.com>
2020-12-09 21:22:06 +0100

    add day 08 part2 JS solution
    Sadly this solution makes use of brute forcing.

--- js/src/08.js
+++ js/src/08.js
@@ -3,10 +3,11 @@ const input = typeof window === 'undefined'
   : document.body.innerText;
 
 const instrExp = /(\w+) ([-+]\d+)/;
-const _part1 = (instructions, index = 0, acc = 0, visited = new Set()) => {
+const _part1 = (instructions, index = 0, acc = 0, visited = new Set(), part2 = false) => {
+  if(index === instructions.length) return acc;
   const instruction = instrExp.exec(instructions[index]);
 
-  if (visited.has(index)) return acc;
+  if (visited.has(index)) return part2 ? false : acc;
   else visited.add(index);
 
   switch (instruction[1]) {
@@ -21,13 +22,35 @@ const _part1 = (instructions, index = 0, acc = 0, visited = new Set()) => {
       break;
   }
 
-  return _part1(instructions, index, acc, visited);
+  return _part1(instructions, index, acc, visited, part2);
 };
 
 const part1 = instructions => _part1(instructions);
+
+// brute forcing it is :(
+const part2 = instrunctions => {
+  let acc;
+
+  for(let i = 0; i < instrunctions.length; i += 1) {
+    const instrs = [...instrunctions];
+
+    if(instrs[i].startsWith('jmp')) {
+      instrs[i] = instrs[i].replace('jmp', 'nop');
+    } else if(instrs[i].startsWith('nop')) {
+      instrs[i] = instrs[i].replace('nop', 'jmp');
+    }
+
+    acc = _part1(instrs, 0, 0, new Set(), true);
+
+    if(acc !== false) break;
+  }
+  
+  return acc;
+};
 
 const list = input.split('\n')
   .filter(i => i != '');
 const testList = ['nop +0', 'acc +1', 'jmp +4', 'acc +3', 'jmp -3', 'acc -99', 'acc +1', 'jmp -4', 'acc +6'];
 
 console.log('Part 1:', part1(list));
+console.log('Part 2:', part2(list));
