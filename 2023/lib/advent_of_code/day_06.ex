193c1c2c7bc9978964a1e3b87193806f677d6940
Joost van Someren <joost.v.someren@gmail.com>
2023-12-06 08:45:08 +0100

    Complete day 06

--- 2023/lib/advent_of_code/day_06.ex
+++ 2023/lib/advent_of_code/day_06.ex
@@ -1,7 +1,48 @@
 defmodule AdventOfCode.Day06 do
-  def part1(_args) do
+  def part1(input) do
+    input
+    |> parse()
+    |> Enum.zip()
+    |> Enum.map(&calculate_distances/1)
+    |> Enum.map(&filter_winning_distances/1)
+    |> Enum.map(&Enum.count/1)
+    |> Enum.product()
   end
 
-  def part2(_args) do
+  def part2(input) do
+    input
+    |> parse()
+    |> Enum.map(&Enum.join/1)
+    |> Enum.map(&String.to_integer/1)
+    |> then(fn [time, distance] -> {time, distance} end)
+    |> calculate_distances()
+    |> filter_winning_distances()
+    |> Enum.count()
+  end
+
+  defp calculate_distances({time, distance}) do
+    1..time
+    |> Enum.map(fn speed ->
+      speed * (time - speed)
+    end)
+    |> then(&{&1, time, distance})
+  end
+
+  defp filter_winning_distances({distances, _, distance}) do
+    distances
+    |> Enum.filter(fn d -> d > distance end)
+  end
+
+  defp parse(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_line/1)
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.split(" ", trim: true)
+    |> tl()
+    |> Enum.map(&String.to_integer/1)
   end
 end
