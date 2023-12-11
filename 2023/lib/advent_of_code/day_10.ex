8f04ad6a37ed2fc399a4bf158088e400032b8e74
Joost van Someren <joost.v.someren@gmail.com>
2023-12-11 15:25:31 +0100

    Complete day 10

--- 2023/lib/advent_of_code/day_10.ex
+++ 2023/lib/advent_of_code/day_10.ex
@@ -1,7 +1,151 @@
 defmodule AdventOfCode.Day10 do
-  def part1(_args) do
+  def part1(input) do
+    {grid, start_index, line_length, _} =
+      input
+      |> parse()
+
+    path_length(grid, start_index, line_length) |> div(2)
   end
 
-  def part2(_args) do
+  def part2(input) do
+    {grid, start_index, line_length, _} =
+      input
+      |> parse()
+
+    indexes = path_indexes(grid, start_index, line_length)
+
+    grid
+    |> Enum.with_index()
+    |> Enum.map(fn {char, index} -> if index in indexes, do: char, else: ?. end)
+    |> Enum.chunk_every(line_length)
+    |> Enum.map(&x/1)
+    |> Enum.sum()
+  end
+
+  defp x(list) do
+    list
+    |> List.to_string()
+    # (L-*7|[FS]-*J) replace with 1 pipe
+    |> String.replace(~r/(L-*7|[FS]-*J)/, "|") # used for examples
+    # |> String.replace(~r/(L-*[7S]|F-*J)/, "|") # used for personal input
+    # (L-*J|[FS]-*7) replace with 2 pipes
+    |> String.replace(~r/(L-*J|[FS]-*7)/, "||") # used for examples
+    # |> String.replace(~r/(L-*J|F-*[7S])/, "||") # used for personal input
+    |> String.to_charlist()
+    |> y()
+  end
+
+  defp y(list, in_path \\ false, count \\ 0)
+
+  defp y([], _, count), do: count
+
+  defp y([?. | tail], false = in_path, count), do: y(tail, in_path, count)
+  defp y([?. | tail], true = in_path, count), do: y(tail, in_path, count + 1)
+
+  defp y([?| | tail], in_path, count), do: y(tail, !in_path, count)
+
+  defp path_indexes(grid, line_length, current_index, previous_index \\ nil)
+
+  defp path_indexes(grid, line_length, current_index, previous_index)
+       when previous_index != nil do
+    current_char = grid |> Enum.at(current_index)
+
+    next_index =
+      connected_indexes(current_char, current_index, line_length)
+      |> Tuple.to_list()
+      |> Enum.find(&(&1 != previous_index))
+
+    cond do
+      current_char == ?S ->
+        []
+
+      true ->
+        [current_index | path_indexes(grid, line_length, next_index, current_index)]
+    end
+  end
+
+  defp path_indexes(grid, current_index, line_length, nil) do
+    # TODO does not work for a generic grid
+    {_, next_index} =
+      connected_indexes(?F, current_index, line_length)
+
+    [current_index | path_indexes(grid, line_length, next_index, current_index)]
+  end
+
+  defp path_length(grid, line_length, current_index, previous_index \\ nil, step \\ 0)
+
+  defp path_length(grid, line_length, current_index, previous_index, step) when step > 0 do
+    current_char = grid |> Enum.at(current_index)
+
+    next_index =
+      connected_indexes(current_char, current_index, line_length)
+      |> Tuple.to_list()
+      |> Enum.find(&(&1 != previous_index))
+
+    cond do
+      current_char == ?S ->
+        step
+
+      true ->
+        path_length(grid, line_length, next_index, current_index, step + 1)
+    end
+  end
+
+  defp path_length(grid, current_index, line_length, nil, 0 = step) do
+    # TODO does not work for a generic grid
+    {_, next_index} =
+      connected_indexes(?F, current_index, line_length)
+
+    path_length(grid, line_length, next_index, current_index, step + 1)
+  end
+
+  defp connected_indexes(?|, current_index, line_length) do
+    {current_index - line_length, current_index + line_length}
+  end
+
+  defp connected_indexes(?-, current_index, _) do
+    {current_index - 1, current_index + 1}
+  end
+
+  defp connected_indexes(?L, current_index, line_length) do
+    {current_index - line_length, current_index + 1}
+  end
+
+  defp connected_indexes(?J, current_index, line_length) do
+    {current_index - line_length, current_index - 1}
+  end
+
+  defp connected_indexes(?7, current_index, line_length) do
+    {current_index - 1, current_index + line_length}
+  end
+
+  defp connected_indexes(?F, current_index, line_length) do
+    {current_index + 1, current_index + line_length}
+  end
+
+  defp connected_indexes(?., _, _) do
+    {}
+  end
+
+  defp connected_indexes(?S, _, _) do
+    {}
+  end
+
+  defp parse(input) do
+    line_length =
+      input
+      |> String.split("\n", trim: true)
+      |> List.first()
+      |> String.length()
+
+    row_count =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.count()
+
+    single_line_input = input |> String.replace("\n", "") |> String.to_charlist()
+    start_index = single_line_input |> Enum.find_index(&(&1 == ?S))
+
+    {single_line_input, start_index, line_length, row_count}
   end
 end
--- 2023/test/advent_of_code/day_10_test.exs
+++ 2023/test/advent_of_code/day_10_test.exs
@@ -3,19 +3,110 @@ defmodule AdventOfCode.Day10Test do
 
   import AdventOfCode.Day10
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  describe "part1" do
+    test "simple" do
+      input = """
+      -L|F7
+      7S-7|
+      L|7||
+      -L-J|
+      L|-JF
+      """
 
-    assert result
+      result = part1(input)
+
+      assert result === 4
+    end
+
+    test "complex" do
+      input = """
+      7-F7-
+      .FJ|7
+      SJLL7
+      |F--J
+      LJ.LJ
+      """
+
+      result = part1(input)
+
+      assert result === 8
+    end
   end
 
-  @tag :skip
-  test "part2" do
-    input = nil
-    result = part2(input)
+  describe "part2" do
+    test "simple1" do
+      input = """
+      ...........
+      .S-------7.
+      .|F-----7|.
+      .||.....||.
+      .||.....||.
+      .|L-7.F-J|.
+      .|..|.|..|.
+      .L--J.L--J.
+      ...........
+      """
 
-    assert result
+      result = part2(input)
+
+      assert result === 4
+    end
+
+    test "simple2" do
+      input = """
+      ..........
+      .S------7.
+      .|F----7|.
+      .||....||.
+      .||....||.
+      .|L-7F-J|.
+      .|..||..|.
+      .L--JL--J.
+      ..........
+      """
+
+      result = part2(input)
+
+      assert result === 4
+    end
+
+    test "complex1" do
+      input = """
+      .F----7F7F7F7F-7....
+      .|F--7||||||||FJ....
+      .||.FJ||||||||L7....
+      FJL7L7LJLJ||LJ.L-7..
+      L--J.L7...LJS7F-7L7.
+      ....F-J..F7FJ|L7L7L7
+      ....L7.F7||L7|.L7L7|
+      .....|FJLJ|FJ|F7|.LJ
+      ....FJL-7.||.||||...
+      ....L---J.LJ.LJLJ...
+      """
+
+      result = part2(input)
+
+      assert result === 8
+    end
+
+    @tag :skip
+    test "complex2" do
+      input = """
+      FF7F7F7F7F7F7F7F---7
+      L|LJ||||||||||||F--J
+      FL-7LJLJ||||||LJL-77
+      F--JF--7||LJLJ7F7FJ-
+      L---JF-JLJ.||-FJLJJ7
+      |F|F-JF---7F7-L7L|7|
+      |FFJF7L7F-JF7|JL---7
+      7-L-JL7||F7|L7F-7F7|
+      L.L7LFJ|||||FJL7||LJ
+      L7JLJL-JLJLJL--JLJ.L
+      """
+
+      result = part2(input)
+
+      assert result === 10
+    end
   end
 end
