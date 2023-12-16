d2fed193840900b7f44aefb6f2180400d741e12b
Joost van Someren <joost.v.someren@gmail.com>
2023-12-16 14:19:09 +0100

    Complete day 15

--- 2023/lib/advent_of_code/day_15.ex
+++ 2023/lib/advent_of_code/day_15.ex
@@ -1,7 +1,80 @@
 defmodule AdventOfCode.Day15 do
-  def part1(_args) do
+  def part1(input) do
+    input
+    |> parse()
+    |> Enum.map(&hash/1)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(input) do
+    input
+    |> parse()
+    |> Enum.reduce(%{}, &hashmap/2)
+    |> Enum.flat_map(&box_power/1)
+    |> Enum.sum()
   end
+
+  defp hashmap(instruction, boxes) do
+    instruction
+    |> String.split(["=", "-"], trim: true)
+    |> do_operation(boxes)
+  end
+
+  defp box_power({box_number, lenses}) do
+    lenses
+    |> Enum.reverse()
+    |> Enum.with_index(1)
+    |> Enum.map(fn {{_, focal_length}, index} -> (box_number + 1) * index * focal_length end)
+  end
+
+  # handles `-` operations
+  defp do_operation([label], boxes) do
+    target_box = hash(label)
+
+    boxes
+    |> Map.get_and_update(target_box, fn
+      nil ->
+        {nil, []}
+
+      list ->
+        {list, Enum.filter(list, fn {l, _} -> l != label end)}
+    end)
+    |> elem(1)
+  end
+
+  # handles `=` operations
+  defp do_operation([label, focal_length], boxes) do
+    target_box = hash(label)
+    focal_length = focal_length |> String.to_integer()
+    lens = {label, focal_length}
+
+    boxes
+    |> Map.get_and_update(target_box, fn
+      nil ->
+        {nil, [lens]}
+
+      list ->
+        index = list |> Enum.find_index(fn {l, _} -> l == label end)
+
+        case index do
+          nil -> {list, [lens | list]}
+          _ -> {list, List.replace_at(list, index, lens)}
+        end
+    end)
+    |> elem(1)
+  end
+
+  defp hash(string, value \\ 0)
+
+  defp hash("", value), do: value
+
+  defp hash(<<char::utf8, rest::binary>>, value) do
+    value
+    |> Kernel.+(char)
+    |> Kernel.*(17)
+    |> rem(256)
+    |> then(&hash(rest, &1))
+  end
+
+  defp parse(input), do: input |> String.replace("\n", "") |> String.split(",")
 end
--- 2023/test/advent_of_code/day_15_test.exs
+++ 2023/test/advent_of_code/day_15_test.exs
@@ -3,19 +3,24 @@ defmodule AdventOfCode.Day15Test do
 
   import AdventOfCode.Day15
 
-  @tag :skip
-  test "part1" do
-    input = nil
+  test "hash" do
+    input = "HASH"
     result = part1(input)
 
-    assert result
+    assert result == 52
   end
 
-  @tag :skip
+  test "part1" do
+    input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
+    result = part1(input)
+
+    assert result == 1320
+  end
+
   test "part2" do
-    input = nil
+    input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
     result = part2(input)
 
-    assert result
+    assert result == 145
   end
 end
